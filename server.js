deeee5a10ad9ab6f6f103320076f1d5de362603e
Mark Vandenhoff <63995457+markvandenhoff@users.noreply.github.com>
2023-12-10 23:21:18 +1100

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('./dbConnection');
 let router = require('./routers/router');
 let http = require('http').createServer(app);
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use('/api/cat',router);
