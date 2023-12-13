547a45dde240ee8dd39cc60b3338a9fd3dfe8cc2
markvandenhoff <mark.vandenhoff@hotmail.com>
2023-12-13 14:36:20 +1100

    commits

--- public/js/script.js
+++ public/js/script.js
@@ -3,7 +3,7 @@ const addCards = (items) => {
         let itemToAppend = '<div class="col s4 center-align">'+
                 '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.path+'">'+
                 '</div><div class="card-content">'+
-                '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#"></a></p></div>'+
+                '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
                 '<div class="card-reveal">'+
                 '<span class="card-title grey-text text-darken-4">'+item.subTitle+'<i class="material-icons right">close</i></span>'+
                 '<p class="card-text">'+item.description+'</p>'+
@@ -15,44 +15,43 @@ const addCards = (items) => {
 const formSubmitted = () => {
     let formData = {};
     formData.title = $('#title').val();
-    formData.subTitle = $('#subTitle').val();
     formData.path = $('#path').val();
+    formData.subTitle = $('#subTitle').val();
     formData.description = $('#description').val();
 
     console.log(formData);
     postCat(formData);
 }
 
-function postCat(cat){
+function postCat(cat) {
     $.ajax({
         url:'/api/cat',
         type:'POST',
         data:cat,
-        success: (result)=>{
+        success: (result) => {
             if (result.statusCode === 201) {
-                alert('cat post successful');
+                alert('cat posted');
+                location.reload();
             }
         }
     });
 }
 
-function getAllCats(){
-    $.get('/api/cat', (response)=>{
-        // response's data is in array format, so we can use it
-        if (response.statusCode === 200) {
-            addCards(response.data);
+function getAllCats() {
+    $.get('/api/cat',(result)=>{
+        if (result.statusCode === 200) {
+            addCards(result.data);
         }
     });
 }
+
 
 $(document).ready(function(){
     $('.materialboxed').materialbox();
     $('#formSubmit').click(()=>{
         formSubmitted();
-        console.log('here');
     });
     $('.modal').modal();
     getAllCats();
     console.log('ready');
 });
-
--- server.js
+++ server.js
@@ -3,12 +3,12 @@ let app = express();
 let port = process.env.port || 3000;
 require('./dbConnection');
 let router = require('./routers/router');
-let http = require('http').createServer(app);
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use('/api/cat',router);
+
 
 http.listen(port, ()=>{
     console.log('express server started');
