c901b0d58cb4feca15397145c3c2495ab1fdb9a5
markvandenhoff <mark.vandenhoff@hotmail.com>
2023-12-13 16:29:20 +1100

    commits

--- public/js/script.js
+++ public/js/script.js
@@ -3,10 +3,10 @@ const addCards = (items) => {
         let itemToAppend = '<div class="col s4 center-align">'+
                 '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.path+'">'+
                 '</div><div class="card-content">'+
-                '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
+                '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p></p></div>'+
                 '<div class="card-reveal">'+
                 '<span class="card-title grey-text text-darken-4">'+item.subTitle+'<i class="material-icons right">close</i></span>'+
-                '<p class="card-text">'+item.description+'</p>'+
+                '<p class="card-text grey-text text-darken-4">'+item.description+'</p>'+
                 '</div></div></div>';
         $("#card-section").append(itemToAppend)
     });
@@ -41,6 +41,7 @@ function getAllCats() {
     $.get('/api/cat',(result)=>{
         if (result.statusCode === 200) {
             addCards(result.data);
+            console.log('we got the cats');
         }
     });
 }
