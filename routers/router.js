e11f95f3de08906f2559d74c6988d323e6b1f2b1
markvandenhoff <mark.vandenhoff@hotmail.com>
2023-12-13 14:30:31 +1100

    updates

--- routers/router.js
+++ routers/router.js
@@ -2,9 +2,9 @@ let express = require('express');
 let router = express.Router();
 let controller = require('../controllers/controller');
 
-router.post('/', function(req, res){
+router.post('/', function(req,res){
     controller.postCat(req,res);
-})
+});
 
 router.get('/', (req,res)=>{
     controller.getAllCats(req,res);
